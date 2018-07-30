1861b7699b399e5df0a1f7628bbeef88faf5bb46
james braun <braun.james.robert@gmail.com>
2018-07-29 19:55:16 -0500

    changes

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
 $.ajax({
-    url: 'https://randomuser.me/api/?results=12',
+    url: 'https://randomuser.me/api/?results=12&?nat=us',
     dataType: 'json',
     success: function(data) {
       console.log(data);
       for (i = 0; i < data.results.length; ++i) {
         $(`.person-${i} > img`).attr('src', `${data.results[i].picture.thumbnail}`);
-        console.log($(`person-0`).outerHTML);
-        $(`.person-${i} > div[class="name"]`).innerHTML = data.results[i].name.first + " " + data.results[i].name.last;
+        $(`.person-${i} .name`).html(data.results[i].name.first + " " + data.results[i].name.last);
+        $(`.person-${i} .email`).html(data.results[i].email);
+        $(`.person-${i} .city`).html(data.results[i].location.city);
+        console.log($(`.name`).innerHTML);
       }
     }
   });
